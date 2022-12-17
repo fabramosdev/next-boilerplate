87336badcb06757f32e1a9358d8e755d814243f3
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2022-12-17 20:38:16 -0300

    Add styled Component in Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,20 @@
-const Main = () => {
-  return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
-  )
-}
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frenta para uma tela com código"
+    />
+  </S.Wrapper>
+)
 
 export default Main
