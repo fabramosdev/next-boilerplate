67d885a518340b428bcea90f1faa2ca62a0b8d5d
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2022-12-17 23:56:02 -0300

    pwa added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
+
+const withPWA = require('next-pwa')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd,
+  },
+})
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>NextJS - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A NextJS boilerplate whith Typescript and Styled Components"
