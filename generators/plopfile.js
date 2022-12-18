3426e849d38b7ba759d6764492e7d06c099d1004
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2022-12-18 18:24:52 -0300

    Add PLOP generator HBS

---
+++ generators/plopfile.js
@@ -0,0 +1,35 @@
+module.exports = function (plop) {
+  // component generator
+  plop.setGenerator('component', {
+    description: 'application controller logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Nome do componente:',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs',
+      },
+    ],
+  })
+}
